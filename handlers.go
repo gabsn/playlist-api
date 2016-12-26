fd2cd385d79b4873f7c9635fb5c4a434e3032ee2
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-26 19:54:43 +0100

    Complete PostSongHandler

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 // HTTP GET /api/songs
@@ -10,18 +11,26 @@ func GetSongHandler(w http.ResponseWriter, r *http.Request) {
 	var songs []Song
 	for _, v := range playlist {
 		songs = append(songs, v)
-    }
+	}
 	w.Header().Set("Content-Type", "apllication/json")
-    j, err := json.Marshal(songs)
+	j, err := json.Marshal(songs)
 	if err != nil {
 		panic(err)
 	}
-	w.WriteHeader(http.StatusOK)
 	w.Write(j)
 }
 
 // HTTP POST /api/songs
-func PostSongHandler(w http.ResponseWriter, r *http.Request) {}
+func PostSongHandler(w http.ResponseWriter, r *http.Request) {
+	var song Song
+	err := json.NewDecoder(r.Body).Decode(&song)
+	if err != nil {
+		panic(err)
+	}
+	song.AddedOn = time.Now()
+	k := song.AddedOn.String()
+	playlist[k] = song
+}
 
 // HTTP PUT /api/songs/{id}
 func PutSongHandler(w http.ResponseWriter, r *http.Request) {}
