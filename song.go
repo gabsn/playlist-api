362d37e1ea3948e1a13ba3d7b92a14981f208fdd
Gabin Marignier <gabin.marignier@gmail.com>
2016-12-26 18:44:34 +0100

    Add data structure

---
+++ song.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"time"
+)
+
+type Song struct {
+	Title   string    `json:"title"`
+	Artist  string    `json:"artist"`
+	AddedOn time.Time `json:"addedon"`
+}
+
+var playlist = make(map[string]Song)
